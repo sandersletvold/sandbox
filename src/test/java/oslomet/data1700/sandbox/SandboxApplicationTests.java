7bd2ca27fa797d888690483f16150012bf6fa4fb
sandersletvold <143513259+sandersletvold@users.noreply.github.com>
2024-04-10 16:02:29 +0200

    Lastet opp filstruktur.

---
+++ src/test/java/oslomet/data1700/sandbox/SandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.data1700.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
